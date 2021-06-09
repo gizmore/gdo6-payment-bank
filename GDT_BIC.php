9a212e9003cbb4fd3afa5c6204fe21cc0ea64fe9
gizmore <gizmore@wechall.net>
2021-06-09 21:52:20 +0200

    6.10.4-r1163

--- GDT_BIC.php
+++ GDT_BIC.php
@@ -3,7 +3,31 @@ namespace GDO\PaymentBank;
 
 use GDO\DB\GDT_String;
 
+/**
+ * Bank Identifier Code.
+ * 
+ * @author gizmore
+ * @version 6.10.4
+ * @since 6.10.1
+ */
 final class GDT_BIC extends GDT_String
 {
 	public function defaultLabel() { return $this->label('bic'); }
+	
+	protected function __construct()
+	{
+	    parent::__construct();
+	    $this->max(34);
+	    $this->ascii();
+	    $this->caseI();
+	}
+	
+	public function validate($value)
+	{
+	    if (parent::validate($value))
+	    {
+	        return true; # @todo Implement BIC check
+	    }
+	}
+	
 }
---
+++ GDT_BLZ.php
@@ -0,0 +1,22 @@
+<?php
+namespace GDO\PaymentBank;
+
+use GDO\DB\GDT_Char;
+
+/**
+ * Bank identifier.
+ * @author gizmore
+ * @since 6.10.4
+ */
+final class GDT_BLZ extends GDT_Char
+{
+    protected function __construct()
+    {
+        parent::__construct();
+        $this->min(8);
+        $this->max(8);
+        $this->ascii();
+        $this->caseS();
+    }
+    
+}
